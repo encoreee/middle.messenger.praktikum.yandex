260f2a651ae258620dcf1bdb559b391ac679a8d9
Alex Raykov <alexraykov2000@gmail.com>
2023-02-11 00:29:12 +0300

    Первая правка замечаний

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
 const express = require('express');
+
 const PORT = 3000;
 
 const app = express();
 
 app.use("/",express.static(`${__dirname}/dist`));
 
-app.listen(PORT, () => {
-  console.log(`Приложение запущено на порту: ${PORT}!`);
+app.listen(process.env.API_PORT || PORT, () => {
+  console.log(`Приложение запущено на порту: ${process.env.API_PORT || PORT}!`);
   console.log(`${__dirname}/dist`)
 });
