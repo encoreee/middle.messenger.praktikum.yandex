0368ffd6ee061009501abd441c71d2502bf0b86e
Aleksandr Raykov <Aleksandr.Raykov@miningelement.com>
2023-02-19 16:52:29 +0300

    Исправления eslint, под wind

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-require("dotenv").config();
+require('dotenv').config();
 const express = require('express');
 
 const PORT = 3000;
-
 const app = express();
-
-app.use("/",express.static(`${__dirname}/dist`));
+app.use('/', express.static(`${__dirname}/dist`));
 
 app.listen(process.env.API_PORT || PORT, () => {
-  console.log(`Приложение запущено на порту: ${process.env.API_PORT || PORT}!`);
-  console.log(`${__dirname}/dist`)
+  // console.log(`Приложение запущено на порту: ${process.env.API_PORT || PORT}!`);
+  // console.log(`${__dirname}/dist`);
 });
