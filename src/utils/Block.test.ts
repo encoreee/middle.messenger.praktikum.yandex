971a403f77ad082916b4fbd9e28aaa071794b433
Aleksandr Raykov <Aleksandr.Raykov@miningelement.com>
2023-03-27 14:36:32 +0300

    Немного тестирования

---
+++ src/utils/Block.test.ts
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import proxyquire from 'proxyquire';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import type BlockType from './Block';
+
+const eventBusMock = {
+  on: sinon.fake(),
+  emit: sinon.fake(),
+};
+
+const { default: Block } = proxyquire('./Block', {
+  './EventBus': {
+    EventBus: class {
+      emit = eventBusMock.emit;
+
+      on = eventBusMock.on;
+    },
+  },
+}) as { default: typeof BlockType };
+
+describe('Block', () => {
+  class ComponentMock extends Block {}
+
+  it('should fire init event on initialization', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const mock = new ComponentMock({});
+    expect(eventBusMock.emit.calledWith('init')).to.eq(true);
+  });
+});
